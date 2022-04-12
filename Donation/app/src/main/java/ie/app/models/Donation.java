eb0fe18cef61d45b2f9f416a129b11b948f1581b
19020442 <ltthanh304@gmail.com>
2022-04-12 10:40:47 +0700

    12/04/2022

---
+++ Donation/app/src/main/java/ie/app/models/Donation.java
@@ -0,0 +1,33 @@
+package ie.app.models;
+
+public class Donation
+{
+    public String _id;
+    public int amount;
+    public String paymenttype;
+    public int upvotes;
+    public Donation (String _id ,int amount, String paymenttype, int upvotes)
+    {
+        this.amount = amount;
+        this.paymenttype = paymenttype;
+        this.upvotes = upvotes;
+        this._id = _id;
+    }
+    public Donation ()
+    {
+        this.amount = 0;
+        this.paymenttype = "";
+        this.upvotes = 0;
+    }
+
+    public Donation(int donatedAmount, String method, int i) {
+        this.amount = donatedAmount;
+        this.paymenttype = method;
+        this.upvotes = i;
+    }
+
+    public String toString()
+    {
+        return _id + ", " + amount + ", " + paymenttype + ", " + upvotes;
+    }
+}
